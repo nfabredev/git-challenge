250dec3af1160f4713ccfc0584140f3502e69238
Nicolas Fabre <nfabredev@gmail.com>
2021-10-10 16:04:08 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
+use flate2::read::ZlibDecoder;
+use flate2::Compression;
 use std::env;
 use std::fs;
-use flate2::read::ZlibDecoder;
 use std::io::Read;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args[1] == "init" {
         init();
-    }
-    else if args[1] == "cat-file" {
+    } else if args[1] == "cat-file" {
         let blob_sha = &args[3];
         cat_file(blob_sha);
+    } else if args[1] == "hash-object" {
+        let file_name = &args[3];
+        hash_object(file_name);
     } else {
         println!("unknown command: {}", args[1]);
     }
@@ -44,6 +47,31 @@ fn cat_file(blob_sha: &String) {
     // that we discard
     let result: Vec<&str> = buffer.split(null_ascii_codepoint).collect()[1];
     print!("{}", result);
+}
+
+fn hash_object(file_name: &String) {
+    let original = File::open(file_name).expect("Unable to open");
+    let mut original_reader = BufReader::new(original);
+
+    let data = encode_file(&mut original_reader).expect("Failed to encode file");
+
+    let encoded = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open("1234567890"); // TODO compute hash name
+
+    let mut encoded_writer = BufWriter::new(encoded);
+    encoded_writer
+        .write_all(&data)
+        .expect("Failed to write encoded file");
+}
+
+fn encode_file(file: &mut Read) -> io::Result<Vec<u8>> {
+    let mut encoded = ZlibEncoder::new(file, Compression::Fast);
+    let mut buffer = Vec::new();
+    encoded.read_to_end(&mut buffer)?;
+    Ok(buffer);
 }
 
 // fn ls(dir_path: String) {
